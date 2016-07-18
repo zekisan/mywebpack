dc4f52da90b2707aae0542ecad474fe6d197288e
Ezequiel <zeki-san@hotmail.com>
2016-07-18 11:39:19 -0300

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = module.exports = {
+  // the base path which will be used to resolve entry points
+  context: __dirname,
+  // the main entry point for our application's frontend JS
+  entry: './app/frontend/javascripts/entry.js',
+};
