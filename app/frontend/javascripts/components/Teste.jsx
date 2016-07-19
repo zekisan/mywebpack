9eddde1b87513ed7f6dfd5c7957c4cd6f46075e6
Ezequiel <zeki-san@hotmail.com>
2016-07-19 11:03:22 -0300

    Added babelrc.

---
+++ app/frontend/javascripts/components/Teste.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+const Teste = React.createClass({
+  render() {
+    return (
+        <div>
+          <p>FUNCIONA!!!</p>
+        </div>
+    )
+  }
+});
+
+export default Teste;
--- app/frontend/javascripts/entry.js
+++ app/frontend/javascripts/entry.js
@@ -1,3 +1,6 @@
+'use strict';
+
+import $ from 'jquery';
 require('expose?$!expose?jQuery!jquery');
 
 var _ = require('lodash');
@@ -6,3 +9,38 @@ _.times(5, function(i) {
 });
 
 alert('oi3');
+
+require('react');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import * as OurComponents from './components';
+
+$.fn.initPlugins = function() {
+  const scope = $(this);
+
+  // react components
+  $('[data-react-class]', scope).each(function() {
+    const $node     = $(this),
+          className = $node.data('reactClass');
+
+    // Assume className is simple and can be found at top-level (global) or in ./components/index.js.
+    let constructor = global[className] || OurComponents[className];
+
+    // Hack to support ES6
+    if (constructor.__esModule && constructor.default)
+      constructor = constructor.default;
+    const props = $node.data('reactProps');
+
+    ReactDOM.render(React.createElement(constructor, props), this);
+  });
+  
+  return this;
+};
+
+
+$(function() {
+  const $body = $('body');
+
+  $body.initPlugins();
+});
